adc146986da9beb34e28a030850063200f525c44
Marcia Rejane <marciarejane230@gmail.com>
2025-08-25 10:08:36 -0300

    chore: atualizar configurações do projeto e App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts, Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
+
+import theme from './src/theme';
+import {Routes} from "./src/Routes"
+import  {Loading } from '@components/Loading';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      
+      <ThemeProvider theme={theme}>
+      {
+        fontsLoaded ? <Routes /> : <Loading />
+      }
+    </ThemeProvider>
+      </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
