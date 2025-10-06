8c0f18b978bfe30fe090011cb8d0efec9f8ab1fa
Marcia Rejane <marciarejane230@gmail.com>
2025-10-06 10:45:26 -0300

    feat: ajustar Header com StatusBar, SafeAreaView e atualização do ícone de voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
-import { Container, Title, BackButton, BackIcon } from './styles';
+import { Container, Title, BackButton, ButtonIcon } from './styles';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../../App';
+import { BellIcon, CaretLeft, CaretLeftIcon } from 'phosphor-react-native';
+import theme from '../../theme';
+
+type AppNavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 type Props = {
   title: string;
+  showBackButton?: boolean;
+
 }
 
-export function Header({ title }: Props) {
-  const navigation = useNavigation();
+export function Header({ title, showBackButton }: Props) {
+  const navigation = useNavigation<AppNavigationProp>();
 
   function handleGoBack() {
     navigation.goBack();
   }
 
   return (
-    <Container>
-      <BackButton onPress={handleGoBack}>
-        <BackIcon />
-      </BackButton>
-      <Title>{title}</Title>
-    </Container>
+    <SafeAreaView>
+      <Container>
+        {showBackButton && (
+          <BackButton onPress={handleGoBack}>
+            <CaretLeftIcon size={32} color="#d9d9d9" />
+          </BackButton>
+        )}
+        <Title>{title}</Title>
+
+        <ButtonIcon
+          onPress={() => navigation.navigate('Notification')}
+        >
+          <BellIcon size={24} color={theme.COLORS.MIDNIGHT_BLUE} />
+        </ButtonIcon>
+      </Container>
+    </SafeAreaView>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,31 +1,30 @@
-import styled from 'styled-components/native';
-import { ArrowLeft } from 'phosphor-react-native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   width: 100%;
-  height: 20%;
-  flex-direction: row;
+  height: 60px;
   align-items: center;
-  justify-content: center; /* Centraliza o título */
-  padding: 25px; /* Espaçamento para a barra de status */
-  background-color: ${({ theme }) => theme.COLORS.MIDNIGHT_BLUE};
+  justify-content: center;
+  flex-direction: row;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
 `;
 
 export const Title = styled.Text`
-  font-size: 20px;
+  margin-top: 30px;
+  font-size: 23px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  flex-direction: row;
-  align-items: center;
+  color: ${({ theme }) => theme.COLORS.DARK_BLUE};
 `;
 
 export const BackButton = styled.TouchableOpacity`
-  position: absolute; 
-  left: 24px;
-  top: 65px;
+  position: absolute;
+  left: 30px;
+  top: 30px;
 `;
 
-export const BackIcon = styled(ArrowLeft).attrs(({ theme }) => ({
-  size: 24,
-  color: theme.COLORS.WHITE,
-}))``;
+export const ButtonIcon = styled.TouchableOpacity`
+  padding: 5px;
+  position: absolute;
+  top: 10px;
+  right: 25px;
+`;
