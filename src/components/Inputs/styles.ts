b1ea8e137172fefa6363057017943b0119a3bfa0
Marcia Rejane <marciarejane230@gmail.com>
2025-10-06 10:49:41 -0300

    style: atualizar estilo do Input com fundo branco

--- src/components/Inputs/styles.ts
+++ src/components/Inputs/styles.ts
@@ -1,7 +1,6 @@
 import theme from "../../theme";
 import styled from "styled-components/native";
 
-
 export const Container = styled.View`
   width: 100%;
   margin-bottom: 10%;
@@ -13,7 +12,8 @@ export const Inputs = styled.TextInput`
   padding: 12px 15px;
   border-radius: 20px;
   border-width: 1px;
-  border-color: ${({theme}) => theme.COLORS.DARK_BLUE};
-  font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  border-color: ${({ theme }) => theme.COLORS.DARK_BLUE};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   font-size: 16px;
 `;
